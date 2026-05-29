01a02693f4070ae8968bec1018c101560f4cb0e1
erichuiza <eric.huiza@pucp.edu.pe>
2026-05-28 19:08:40 -0500

    se agregó un controlador de estado del Endpoint y una modificación en el script SQL

---
+++ backend/src/main/java/pe/edu/pucp/tiendavirtual/api/HealthCheckController.java
@@ -0,0 +1,12 @@
+package pe.edu.pucp.tiendavirtual.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/")
+    public String health() {
+        return "OK";
+    }
+}
